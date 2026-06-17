ed6f8bcdf7f8db8ec0ee2a2721cc4b314083fba4
beranekleon <leon.beranek06@gmail.com>
2026-06-17 18:19:37 +0200

    Setup Astro, Bun and CI/CD pipeline

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Your GitHub URL will be: https://<username>.github.io/<repo-name>
+  site: 'https://beranekleon.github.io',
+  base: '/beranekleon.github.io', 
+});
---
+++ src/pages/posts/hello-world.md
@@ -0,0 +1,8 @@
+---
+layout: ../../layouts/Layout.astro
+title: Hello World From Markdown!
+---
+
+# Hello World
+
+This is my very first markdown page compiled automatically using Bun and Astro!
